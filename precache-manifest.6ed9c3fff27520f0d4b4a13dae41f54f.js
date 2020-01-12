742f46ba81d8192bdd24b1d51d8c2e64a7475570
donaldshen <donaldshen@users.noreply.github.com>
2020-01-12 12:40:21 +0000

    Deploying to gh-pages from  4e37f0ca98ef6873c4f4bb5f7b9a9ed978a0081e

---
+++ precache-manifest.6ed9c3fff27520f0d4b4a13dae41f54f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b0fb21ead0b2be5044",
+    "url": "/css/app.ad3e0d88.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c4953457d88ce35459d43b0d615e9aa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48b0fb21ead0b2be5044",
+    "url": "/js/app.9aeaa725.js"
+  },
+  {
+    "revision": "6a113723971bdaf963ee",
+    "url": "/js/chunk-vendors.391578d2.js"
+  },
+  {
+    "revision": "5d45d4a5cf4dd2f1b7b11a7ae1d4ac40",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
