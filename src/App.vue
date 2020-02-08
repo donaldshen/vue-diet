9bd876451f10b23ab4ae09126136b78bccba1c16
donald <825870831@qq.com>
2020-02-08 19:07:59 +0800

    Add confirm dialog

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <v-app>
-    <v-card width="400" class="mx-auto mt-5" hover>
+  <v-app class="grey">
+    <v-card
+      width="400"
+      :class="['mx-auto', {'mt-5': bp.smAndUp, 'flex-grow-1': bp.xs}]"
+      raised
+      :flat="bp.xs"
+    >
       <v-card-title class="pb-0">
         <h2>数据表</h2>
       </v-card-title>
@@ -33,17 +38,20 @@
           :label="`减肥速率: ${dietRatio}%`"
           inverse-label
         />
-        <div>每日可摄取：{{ caloriesDaily.toFixed() }} kcal</div>
+        <div>每日可摄取：{{ caloriesDaily.toFixed() }} 千卡</div>
         <div>
           热量盈余：<span
             :class="caloriesSurplus > 0 ? 'green--text' : 'red--text'"
-            >{{ caloriesSurplus.toFixed(2) }} kcal</span
+            >{{ caloriesSurplus.toFixed(2) }} 千卡</span
           >
         </div>
       </v-card-text>
-      <v-divider></v-divider>
+      <v-divider />
       <v-card-actions>
-        <v-btn :disabled="!food.name || !food.calories" @click="eat(food)"
+        <v-btn
+          color="success"
+          :disabled="!food.name || !food.calories"
+          @click="eat(food)"
           >摄入</v-btn
         >
         <v-text-field
@@ -56,53 +64,85 @@
           v-model="food.calories"
           label="热量"
           type="number"
-          suffix="kcal"
+          suffix="千卡"
         />
       </v-card-actions>
       <template v-if="history.length">
-        <v-divider></v-divider>
+        <v-divider />
         <v-card-actions>
-          <v-simple-table>
+          <v-simple-table class="flex-grow-1">
+            <thead>
+              <tr>
+                <th>日期</th>
+                <th>食物</th>
+                <th>千卡</th>
+              </tr>
+            </thead>
             <tbody>
-              <tr
+              <v-hover
                 v-for="record in history.slice(-10).reverse()"
                 :key="record.time"
-                @click="eat(record)"
+                v-slot:default="{hover}"
               >
-                <td>{{ new Date(+record.time).toLocaleString() }}</td>
-                <td>{{ record.name }}</td>
-                <td>{{ record.calories }} kcal</td>
-              </tr>
+                <tr
+                  :style="{cursor: hover ? 'pointer' : 'auto'}"
+                  @click="confirm('确认消化吗？', () => eat(record))"
+                >
+                  <td>{{ formatDate(+record.time) }}</td>
+                  <td>{{ record.name }}</td>
+                  <td>{{ record.calories }}</td>
+                </tr>
+              </v-hover>
             </tbody>
           </v-simple-table>
         </v-card-actions>
       </template>
+      <v-divider />
+      <v-card-actions>
+        <v-spacer />
+        <v-btn class="error" @click="reset">重置</v-btn>
+      </v-card-actions>
     </v-card>
+    <v-dialog v-model="confirming" width="300">
+      <v-card>
+        <v-card-title>{{ confirmMsg }}</v-card-title>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="error" @click="confirmCallBack(), (confirming = false)"
+            >确认</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-app>
 </template>
 
 <script>
-const storage = {
-  get(key, defaultValue) {
-    return JSON.parse(localStorage.getItem(key)) || defaultValue
-  },
-  set(key, value) {
-    localStorage.setItem(key, JSON.stringify(value))
-  },
+import dayjs from 'dayjs'
+import store from 'store2'
+
+const defaultData = {
+  weight: 0,
+  activityMutiplier: 28,
+  dietRatio: 0,
+  caloriesSurplus: 0,
+  history: [],
+  lastTime: Date.now(),
 }
+// 非覆盖的方式存到 localStorage
+Object.entries(defaultData).forEach(([key, value]) => store(key, value, false))
 
 export default {
   name: 'App',
   data: () => ({
-    weight: storage.get('weight', 0),
-    activityMutiplier: storage.get('activityMutiplier', 28),
-    dietRatio: storage.get('dietRatio', 0),
-    caloriesSurplus: 0,
-    food: storage.get('food', {
+    ...Object.fromEntries(Object.keys(defaultData).map(k => [k, store(k)])),
+    food: {
       name: '',
       calories: undefined,
-    }),
-    history: storage.get('history', []),
+    },
+    confirming: false,
+    confirmMsg: '',
+    confirmCallBack: () => {},
   }),
   computed: {
     activityDescription: ({activityMutiplier}) =>
@@ -115,31 +155,28 @@ export default {
     caloriesDaily: ({weight, activityMutiplier, dietRatio}) =>
       weight * activityMutiplier * (1 - dietRatio / 100),
     caloriesPerSecond: ({caloriesDaily}) => caloriesDaily / 24 / 60 / 60,
+    bp: ({$vuetify: {breakpoint}}) => breakpoint,
   },
   watch: {
-    weight: v => storage.set('weight', v),
-    activityMutiplier: v => storage.set('activityMutiplier', v),
-    dietRatio: v => storage.set('dietRatio', v),
-    history: v => storage.set('history', v),
+    weight: v => store('weight', v),
+    activityMutiplier: v => store('activityMutiplier', v),
+    dietRatio: v => store('dietRatio', v),
+    history: v => store('history', v),
   },
   created() {
-    this.resetCaloriesSurplus()
+    this.correctCaloriesSurplus()
     this.initTimer()
   },
-  beforeDestroy() {
-    storage.set('caloriesSurplus', [Date.now(), this.caloriesSurplus])
-  },
   methods: {
-    resetCaloriesSurplus() {
-      const [lastTime = Date.now(), lastCaloriesSurplus = 0] = storage.get(
-        'caloriesSurplus',
-        [],
-      )
+    correctCaloriesSurplus() {
+      const lastTime = store('lastTime')
       this.caloriesSurplus =
-        lastCaloriesSurplus +
         (this.caloriesPerSecond * (Date.now() - lastTime)) / 1000
-      document.addEventListener('pagehide', () => {
-        storage.set('caloriesSurplus', [Date.now(), this.caloriesSurplus])
+      document.addEventListener('visibilitychange', () => {
+        if (document.visibilityState === 'hidden') {
+          store('caloriesSurplus', this.caloriesSurplus)
+          store('lastTime', Date.now())
+        }
       })
     },
     initTimer() {
@@ -148,6 +185,17 @@ export default {
     eat(food) {
       this.caloriesSurplus -= food.calories
       this.history.push({...food, time: Date.now()})
+    },
+    formatDate(dateValue) {
+      return dayjs(dateValue).format('YYYY/M/D ha')
+    },
+    confirm(msg, cb) {
+      this.confirmMsg = msg
+      this.confirming = true
+      this.confirmCallBack = cb
+    },
+    reset() {
+      this.confirm('确认重置吗？', () => store(false))
     },
   },
 }
