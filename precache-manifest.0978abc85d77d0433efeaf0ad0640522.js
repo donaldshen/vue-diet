1f8d44b69cb2bf55338ab5b7bc427b402aba0809
donaldshen <donaldshen@users.noreply.github.com>
2020-02-13 10:27:55 +0000

    Deploying to gh-pages from  193b2751b137931f4885c9273d8757b9ceecaf2e

--- precache-manifest.0978abc85d77d0433efeaf0ad0640522.js
+++ precache-manifest.0978abc85d77d0433efeaf0ad0640522.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b5c4d9c39b851bc9ce95",
+    "revision": "e9893850e5e547c8f38c",
     "url": "/vue-diet/css/chunk-vendors.bc58462a.css"
   },
   {
-    "revision": "f9a631969610d5af2ef12343ec902946",
+    "revision": "1a8df65d2120c26cc27c7d02869eb3ee",
     "url": "/vue-diet/index.html"
   },
   {
-    "revision": "58e0f50b5ccab73cc3d9",
-    "url": "/vue-diet/js/app.c9fcbe64.js"
+    "revision": "347721851dcad65c8a1d",
+    "url": "/vue-diet/js/app.a8e9d202.js"
   },
   {
-    "revision": "b5c4d9c39b851bc9ce95",
-    "url": "/vue-diet/js/chunk-vendors.d1df6b69.js"
+    "revision": "e9893850e5e547c8f38c",
+    "url": "/vue-diet/js/chunk-vendors.4fb12dc6.js"
   },
   {
     "revision": "7648f345cbee3e0cc42b7b221b77b5b4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vue-diet/precache-manifest.63066323675cc7b00fa49da2b3792a82.js"
+  "/vue-diet/precache-manifest.0978abc85d77d0433efeaf0ad0640522.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-diet"});
